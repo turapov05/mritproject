7dd643751ec0ff16cfc466ad9e08d8f0cb566985
abdu <torabekturopov1@gmail.com>
2024-07-30 10:58:49 +0500

    Rasm qo'shishni o'rgandik

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class Index(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.IntegerField(max_length=13)
+    photos = models.ImageField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return f"Ismi:{self.last_name}    Familiyasi:{self.last_name}    telefon raqami:{self.phone_number}"
